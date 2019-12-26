b535fda5cd3d2a20cfb97695ce2ab5b230f196ec
pratamawijaya <set.mnemonix@gmail.com>
2019-12-26 16:58:34 +0700

    19 - change getrandom usecase to mockk

--- app/src/test/java/com/pratama/tdd_kotlin/domain/usecases/GetRandomNumberTriviaTest.kt
+++ app/src/test/java/com/pratama/tdd_kotlin/domain/usecases/GetRandomNumberTriviaTest.kt
@@ -1,59 +1,47 @@
 package com.pratama.tdd_kotlin.domain.usecases
 
-import com.nhaarman.mockitokotlin2.verify
-import com.nhaarman.mockitokotlin2.whenever
 import com.pratama.tdd_kotlin.core.data.Result
-import com.pratama.tdd_kotlin.core.functional.Either
 import com.pratama.tdd_kotlin.core.usecase.UseCase
 import com.pratama.tdd_kotlin.domain.entities.NumberTrivia
 import com.pratama.tdd_kotlin.domain.repositories.NumberTriviaRepository
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.junit.MockitoJUnitRunner
+import kotlin.test.assertEquals
 
-@RunWith(MockitoJUnitRunner::class)
 class GetRandomNumberTriviaTest {
+
     private val testDispatcher = TestCoroutineDispatcher()
-
-    private lateinit var getRandomNumberTrivia: GetRandomNumberTrivia
-
-    @Mock
-    private lateinit var repository: NumberTriviaRepository
+    private val repository: NumberTriviaRepository = mockk()
+    private val getRandomNumberTrivia = GetRandomNumberTrivia(repository)
 
     @Before
     fun setUp() {
         Dispatchers.setMain(testDispatcher)
-        getRandomNumberTrivia = GetRandomNumberTrivia(repository)
-    }
-
-    @After
-    fun tearDown() {
-        Dispatchers.resetMain()
     }
 
     @Test
-    fun `getRandomNumberTrivia should return NumberTrivia`() {
+    fun `getRandomNumberTrivia should return NumberTrivia`() = runBlocking {
         val testNumber = 1
-        val testNumberTrivia = NumberTrivia(number = testNumber, text = "Test Text")
+        val testNumberTrivia = NumberTrivia(number = testNumber, text = "Test")
 
-        runBlocking {
-            whenever(repository.getRandomNumberTrivia()).thenReturn(Result.Success(testNumberTrivia))
+        every {
+            runBlocking {
+                repository.getRandomNumberTrivia()
+            }
+        } returns Result.Success(
+            testNumberTrivia
+        )
 
-            val result = getRandomNumberTrivia.execute(UseCase.None())
+        val result = getRandomNumberTrivia.execute(UseCase.None())
 
-            verify(repository).getRandomNumberTrivia()
+        assertEquals(result, Result.Success(testNumberTrivia))
 
-            assertEquals(result, Result.Success(testNumberTrivia))
-        }
     }
 }
